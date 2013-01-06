e59f1842d5ff545646180b4f54481569b6a6a86c
David Litmark <david@litmark.se>
2013-01-06 22:04:28 +0100

    Update to support refactorings in app.js

--- test/spec/app.js
+++ test/spec/app.js
@@ -1,20 +1,23 @@
 describe('#findDistribution', function() {
   it('returns the breakpoints distributed over the range', function() {
-    var output = findDistribution(100);
+    var output = beatsheet.findDistribution(100);
     expect(output).to.eql([1, 4, 8, 9,10, 20, 24, 50, 68, 77, 81, 85, 89, 93, 97, 100]);
   });
 });
 
-describe('#distribute', function() {
-  it('returns a function', function() {
-    var distributor = distribute(2);
-    expect(distributor).to.be.a('Function');
-  });
-});
 
-describe('#distributor', function() {
-  it('multiplies the input w. the predefined number', function() {
-    var distributor = distribute(2);
-    expect(distributor(4)).to.equal(8);
-  });
-});
+// functions below are not testable no more since they are private, unless I use a spy. Remove these from tests?
+
+// describe('#distribute', function() {
+//   it('returns a function', function() {
+//     var distributor = beatsheet.distribute(2);
+//     expect(distributor).to.be.a('Function');
+//   });
+// });
+
+// describe('#distributor', function() {
+//   it('multiplies the input w. the predefined number', function() {
+//     var distributor = beatsheet.distribute(2);
+//     expect(distributor(4)).to.equal(8);
+//   });
+// });
