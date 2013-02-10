67ccdd1eb23ab29dd4c2e00e27bb2b67b1981cac
David Litmark <david@litmark.se>
2013-02-10 14:36:10 +0100

    Adds helper object and refactors the beatsheet obj

--- js/app.js
+++ js/app.js
@@ -18,145 +18,143 @@
     // Establish the root object.
     var root = this;
 
+    var helpers = {
+
+        findBreakpoints : function ( range ) {
+            return [1 / range, 0.04, 0.08, 0.09, 0.1, 0.2, 0.24, 0.5, 0.68, 0.77, 0.81, 0.85, 0.89, 0.93, 0.97, 1];
+        },
+
+        // Function for calculating distributions
+        distribute : function ( range ) {
+            return function ( m ) {
+                return Math.ceil( m * range );
+            };
+        },
+
+        // Find the distribution over the range. The range can be either the page count or word count.
+        findDistribution : function ( range ) {
+            var breakpoints = this.findBreakpoints( range );
+            var distributor = this.distribute( range );
+            return breakpoints.map( distributor );
+        },
+
+        build : function ( outline ) {
+            return [
+                {
+                    "beat" : "The opening image",
+                    "begin" : outline[0],
+                    "end" : "NULL"
+                },
+                {
+                    "beat" : "Theme Stated",
+                    "begin" : outline[1],
+                    "end" : "NULL"
+                },
+                {
+                    "beat" : "Set-up",
+                    "begin" : outline[0],
+                    "end" : outline[2]
+                },
+                {
+                    "beat" : "Statis=Death",
+                    "begin" : outline[3],
+                    "end" : "NULL"
+                },
+                {
+                    "beat" : "Catalyst",
+                    "begin" : outline[4],
+                    "end" : "NULL"
+                },
+                {
+                    "beat" : "Debate",
+                    "begin" : outline[4],
+                    "end" : outline[5]
+                },
+                {
+                    "beat" : "Break into two",
+                    "begin" : outline[5],
+                    "end" : "NULL"
+                },
+                {
+                    "beat" : "B-story",
+                    "begin" : outline[6],
+                    "end" : "NULL"
+                },
+                {
+                    "beat" : "Fun and Games",
+                    "begin" : outline[6],
+                    "end" : outline[7]
+                },
+                {
+                    "beat" : "Midpoint",
+                    "begin" : outline[7],
+                    "end" : "NULL"
+                },
+                {
+                    "beat" : "Bad Guys close in",
+                    "begin" : outline[7],
+                    "end" : outline[8]
+                },
+                {
+                    "beat" : "All Is Lost",
+                    "begin" : outline[8],
+                    "end" : "NULL"
+                },
+                {
+                    "beat" : "Dark Night of the Soul",
+                    "begin" : outline[8],
+                    "end" : outline[9]
+                },
+                {
+                    "beat" : "Break Into Three",
+                    "begin" : outline[9],
+                    "end" : "NULL"
+                },
+                {
+                    "beat" : "Gathering the team",
+                    "begin" : outline[10],
+                    "end" : "NULL"
+                },
+                {
+                    "beat" : "Executing the Plan",
+                    "begin" : outline[11],
+                    "end" : "NULL"
+                },
+                {
+                    "beat" : "The Height Tower Surprise",
+                    "begin" : outline[12],
+                    "end" : "NULL"
+                },
+                {
+                    "beat" : "Dig, Deep Down",
+                    "begin" : outline[13],
+                    "end" : "NULL"
+                },
+                {
+                    "beat" : "The Execution of the New Plan",
+                    "begin" : outline[14],
+                    "end" : "NULL"
+                },
+                {
+                    "beat" : "Final Image",
+                    "begin" : outline[15],
+                    "end" : "NULL"
+                }
+            ];
+        }
+    };
+
     // Create the beatsheet object.
-    var beatsheet = {};
-//    function(size){
-//       return this.generate(size);
-//    };
+    var beatsheet = function ( size ) {
+        var distribution = helpers.findDistribution( size );
+        var result = helpers.build( distribution );
+        return JSON.stringify( result );
+    };
 
     // Export the beatsheet object.
     root.beatsheet = beatsheet;
 
     // Current version
     beatsheet.version = "0.1.0";
-
-    var findBreakpoints = function ( range ) {
-        return [1 / range, 0.04, 0.08, 0.09, 0.1, 0.2, 0.24, 0.5, 0.68, 0.77, 0.81, 0.85, 0.89, 0.93, 0.97, 1];
-    };
-
-    // Internal function for calculating distributions
-    var distribute = function ( range ) {
-        return function ( m ) {
-            return Math.ceil( m * range );
-        };
-    };
-
-    // Find the distribution over the range. The range can be either the page count or word count.
-    beatsheet.findDistribution = function ( range ) {
-        var breakpoints = findBreakpoints( range );
-        var distributor = distribute( range );
-        return breakpoints.map( distributor );
-    };
-
-    beatsheet.build = function ( outline ) {
-        return [
-            {
-                "beat"  : "The opening image",
-                "begin" : outline[0],
-                "end"   : "NULL"
-            },
-            {
-                "beat"  : "Theme Stated",
-                "begin" : outline[1],
-                "end"   : "NULL"
-            },
-            {
-                "beat"  : "Set-up",
-                "begin" : outline[0],
-                "end"   : outline[2]
-            },
-            {
-                "beat"  : "Statis=Death",
-                "begin" : outline[3],
-                "end"   : "NULL"
-            },
-            {
-                "beat"  : "Catalyst",
-                "begin" : outline[4],
-                "end"   : "NULL"
-            },
-            {
-                "beat"  : "Debate",
-                "begin" : outline[4],
-                "end"   : outline[5]
-            },
-            {
-                "beat"  : "Break into two",
-                "begin" : outline[5],
-                "end"   : "NULL"
-            },
-            {
-                "beat"  : "B-story",
-                "begin" : outline[6],
-                "end"   : "NULL"
-            },
-            {
-                "beat"  : "Fun and Games",
-                "begin" : outline[6],
-                "end"   : outline[7]
-            },
-            {
-                "beat"  : "Midpoint",
-                "begin" : outline[7],
-                "end"   : "NULL"
-            },
-            {
-                "beat"  : "Bad Guys close in",
-                "begin" : outline[7],
-                "end"   : outline[8]
-            },
-            {
-                "beat"  : "All Is Lost",
-                "begin" : outline[8],
-                "end"   : "NULL"
-            },
-            {
-                "beat"  : "Dark Night of the Soul",
-                "begin" : outline[8],
-                "end"   : outline[9]
-            },
-            {
-                "beat"  : "Break Into Three",
-                "begin" : outline[9],
-                "end"   : "NULL"
-            },
-            {
-                "beat"  : "Gathering the team",
-                "begin" : outline[10],
-                "end"   : "NULL"
-            },
-            {
-                "beat"  : "Executing the Plan",
-                "begin" : outline[11],
-                "end"   : "NULL"
-            },
-            {
-                "beat"  : "The Height Tower Surprise",
-                "begin" : outline[12],
-                "end"   : "NULL"
-            },
-            {
-                "beat"  : "Dig, Deep Down",
-                "begin" : outline[13],
-                "end"   : "NULL"
-            },
-            {
-                "beat"  : "The Execution of the New Plan",
-                "begin" : outline[14],
-                "end"   : "NULL"
-            },
-            {
-                "beat"  : "Final Image",
-                "begin" : outline[15],
-                "end"   : "NULL"
-            }
-        ];
-    };
-
-    beatsheet.generate = function ( size ) {
-        var distribution = this.findDistribution( size );
-        var result = this.build( distribution );
-        return JSON.stringify( result );
-    };
 
 }).call( this );
--- test/spec/app.js
+++ test/spec/app.js
@@ -6,27 +6,6 @@
     "use strict";
 
     var range = [1, 4, 8, 9, 10, 20, 24, 50, 68, 77, 81, 85, 89, 93, 97, 100];
-    //var expectedOutput =
-    //		"The opening image (1)"
-    //    	+ ",Theme Stated (4)"
-    //    	+ ",Set-up (1 to 8)"
-    //    	+ ",Statis=Death (9)"
-    //    	+ ",Catalyst (10)"
-    //    	+ ",Debate (10 to 20)"
-    //    	+ ",Break into two (20)"
-    //    	+ ",B-story (24)"
-    //    	+ ",Fun and Games (24 to 50)"
-    //    	+ ",Midpoint (50)"
-    //    	+ ",Bad Guys close in (50 to 68)"
-    //    	+ ",All Is Lost (68)"
-    //    	+ ",Dark Night of the Soul (68 to 77)"
-    //    	+ ",Break Into Three (77)"
-    //    	+ ",Gathering the team (81)"
-    //    	+ ",Executing the Plan (85)"
-    //    	+ ",The Hight Tower Suprise (89)"
-    //    	+ ",Dig, Deep Down (93)"
-    //    	+ ",The Execution of the New Plan (97)"
-    //    	+ ",Final Image (100)"
 
     var result = [
         {
@@ -133,12 +112,12 @@
 
     var serializedResult = JSON.stringify(result);
 
-    describe('#findDistribution', function () {
-        it('returns the breakpoints distributed over the range', function () {
-            var output = beatsheet.findDistribution(100);
-            expect(output).to.eql(range);
-        });
-    });
+//    describe('#findDistribution', function () {
+//        it('returns the breakpoints distributed over the range', function () {
+//            var output = beatsheet.findDistribution(100);
+//            expect(output).to.eql(range);
+//        });
+//    });
 
 //    describe('#print', function () {
 //        it('returns a description of the output', function () {
@@ -147,9 +126,9 @@
 //        });
 //    });
 
-    describe('#generate', function () {
+    describe('beatsheet', function () {
         it('returns the beatsheet based the given input', function () {
-            var output = beatsheet.generate(100);
+            var output = beatsheet(100);
             expect(output).to.eql(serializedResult);
         });
     });
