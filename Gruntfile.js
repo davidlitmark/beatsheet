5005d975c5642367e47b43ce8771f7667831b4ac
David Litmark <david@litmark.se>
2013-05-10 23:38:46 +0200

    Replace grunt-mocha w. grunt-karma

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,46 @@
 /*global module:false*/
-module.exports = function ( grunt ) {
+module.exports = function(grunt) {
     "use strict";
 
     // Project configuration.
-    grunt.initConfig( {
-        lint : {
-            files : ['grunt.js', 'test/specs/*.js']
+    grunt.initConfig({
+        lint: {
+            files: ['grunt.js', 'test/specs/*.js']
         },
-        jshint : {
-            options : {
-                curly : true,
-                eqeqeq : true,
-                immed : true,
-                latedef : true,
-                newcap : true,
-                noarg : true,
-                sub : true,
-                undef : true,
-                boss : true,
-                eqnull : true,
-                node : true
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                node: true
             },
-            globals : {
-                it : true,
-                describe : true
+            globals: {
+                it: true,
+                describe: true
             }
         },
-        watch : {
-            test : {
-                files : ['js/*.js', 'test/spec/*.js'],
-                tasks : 'mocha'
+        min: {
+            dist: {
+                src: ['js/app.js'],
+                dest: 'dist/beatsheet.min.js'
             }
         },
-        mocha : {
-            index : ['test/test.html']
-        },
-        min : {
-            dist : {
-                src : ['js/app.js'],
-                dest : 'dist/beatsheet.min.js'
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js'
             }
         }
-    } );
+    });
 
-    grunt.loadNpmTasks( 'grunt-mocha' );
+    grunt.loadNpmTasks('grunt-karma');
 
     // Default task.
-    grunt.registerTask( 'default', 'lint mocha min' );
-    grunt.registerTask( 'test', 'mocha' );
-
-
+    grunt.registerTask('default', 'lint min');
 };
